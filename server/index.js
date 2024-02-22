ef64dd01028aca47b409797f3c643a6580a5c79c
ferrondw <145978195+ferrondw@users.noreply.github.com>
2024-02-22 17:26:43 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,11 +37,14 @@ const UsersState = {
     }
 }
 
-const io = new Server(expressServer, {
+const io = new Server(expressServer)
+/*
+, {
     cors: {
         origin: process.env.NODE_ENV === "production" ? false : ["http://127.0.0.1:3500"]
     }
-})
+}
+*/
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
